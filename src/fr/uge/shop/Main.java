f141c569d303c692dcc24b8f4c93271907c0b120
karabila <karabily@outlook.fr>
2023-05-26 10:29:49 +0200

    Main

--- src/fr/uge/shop/Main.java
+++ src/fr/uge/shop/Main.java
@@ -6,13 +6,13 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Money money1 = new Money();
-        System.out.println(money1);  // 0g 0s 0c
-        Money money2 = money1.add(2, String.valueOf(Money.Coin.GOLD));
-        System.out.println(money2);  // 2g 0s 0c
+        var money1 = new Money();
+
+        var money2 = money1.add(2, String.valueOf(Money.Coin.GOLD));
 
         var money3 = money2.add(3, String.valueOf(Money.Coin.GOLD));
-
+        System.out.println(money1);  // 0g 0s 0c
+        System.out.println(money2);  // 2g 0s 0c
         System.out.println(money3);  // 5g 0s 0c
     }
 }
