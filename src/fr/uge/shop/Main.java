cb1f9319e580b664472c35e63721734f317a6cd2
karabila <karabily@outlook.fr>
2023-05-26 10:48:56 +0200

    adding line  of test TD part2

--- src/fr/uge/shop/Main.java
+++ src/fr/uge/shop/Main.java
@@ -8,11 +8,20 @@ public class Main {
 
         var money1 = new Money();
 
-        var money2 = money1.add(2, String.valueOf(Money.Coin.GOLD));
+        var money2 = money1.add(2, Money.Coin.GOLD);
 
-        var money3 = money2.add(3, String.valueOf(Money.Coin.GOLD));
+        var money3 = money2.add(3, Money.Coin.GOLD);
         System.out.println(money1);  // 0g 0s 0c
         System.out.println(money2);  // 2g 0s 0c
         System.out.println(money3);  // 5g 0s 0c
+        System.out.println("===========================================");
+
+
+        var money4 = Money.ZERO;
+        var money5 = Money.ZERO.add(7, Money.Coin.GOLD);
+        System.out.println(money4.equals(new Money()));  // true
+        System.out.println(money5.equals(Money.ZERO.add(7, Money.Coin.GOLD)));  // true
+        System.out.println(money5.hashCode() == Money.ZERO.add(7, Money.Coin.GOLD).hashCode());  // true
+
     }
 }
